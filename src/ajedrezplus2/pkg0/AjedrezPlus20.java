838cbabe2f3b1616dd6f1795ea2d496fa4db6985
Jesús Rivas <rivas170106@gmail.com>
2024-03-19 01:24:48 -0300

    Inicialización

---
+++ src/ajedrezplus2/pkg0/AjedrezPlus20.java
@@ -0,0 +1,13 @@
+
+package ajedrezplus2.pkg0;
+
+public class AjedrezPlus20 {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+
+    }
+    
+}
